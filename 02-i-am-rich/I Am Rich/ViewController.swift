78c58fab1c0228aa2827a2fc22f169f5627fe58d
Victor <vcarvalho0402@gmail.com>
2019-10-08 23:14:27 -0300

    Add the second project

---
+++ 02-i-am-rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by VIctor Carvalho on 08/10/19.
+//  Copyright © 2019 victor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
